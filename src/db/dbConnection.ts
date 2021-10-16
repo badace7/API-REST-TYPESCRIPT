6d4a437b5348eea8b155b6b48bb066619f2e5384
ali <ali.belhacene@yahoo.fr>
2021-10-16 16:33:06 +0200

    Modifying dotenv and dbConnection files

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const dbName = process.env.DB_NAME as string;
 const dbUser = process.env.DB_USER as string;
 const dbHost = process.env.DB_HOST as string;
-const dbDriver = "mysql" as Dialect;
+const dbDriver = process.env.DB_DRIVER as Dialect;
 const dbPassword = process.env.DB_PASSWORD as string;
 
 
